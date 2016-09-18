1bad92bcb6b2252b0867494498c59d22dc6c1ceb
jefarrell <johnefarrell18@gmail.com>
2016-09-17 20:16:47 -0400

    EON

--- public/javascripts/src/script.js
+++ public/javascripts/src/script.js
@@ -1,1 +1,10 @@
 console.log(' checking in from script');
+
+var offset = 50;
+
+$('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
+
