cb73de1919550a9626a665d4480cb24e6123bbc4
jefarrell <johnefarrell18@gmail.com>
2016-09-18 15:50:10 -0400

    configs

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+//const MongoClient = require('mongodb').MongoClient;
+//const assert = require('assert');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const server = app.listen(3000, function() {
-	const host = server.address().address;
-	const port = server.address().port;
-	console.log("Server listening at http://%s:%s", host, port);
-});
-
 const url = 'mongodb://127.0.0.1:27017/MeshYou'
-MongoClient.connect(url, (err, db) => {
-	assert.equal(null,err);
-	console.log("database connected");
 
-	db.close();
+// MongoClient.connect(url, (err, db) => {
+// 	assert.equal(null,err);
+// 	console.log("Database Connected");
+// 	const server = app.listen(3000, () => {
+// 		const host = server.address().address;
+// 		const port = server.address().port;
+// 		console.log("Server listening at http://%s:%s", host, port);
+// 	});
+// });
+
+mongoose.connect(url);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+	console.log("Database open")
+	const server = app.listen(3000, () => {
+		const host = server.address().address;
+		const port = server.address().port;
+		console.log("Server listening at %s%s", host, port);
+	});	
+
 });
-
 
 app.use(express.static(path.join(__dirname, '/dist/')));
 app.use(express.static(path.join(__dirname, '/public/assets')));
